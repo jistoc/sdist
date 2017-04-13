c0bf94a77cd037f0cd4f6dd65d6435c7b3b2e1fc
satna <rafael.amaio@gmail.com>
2017-04-13 03:47:12 -0300

    Primeira Versão

---
+++ src/exceptions/UserException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author user
+ */
+public class UserException extends Exception {
+
+    /**
+     * Creates a new instance of <code>UserException</code> without detail
+     * message.
+     */
+    public UserException() {
+    }
+
+    /**
+     * Constructs an instance of <code>UserException</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public UserException(String msg) {
+        super(msg);
+    }
+}
